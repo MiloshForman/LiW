e9778f2fd05b1633bd79a478b64eebbaedd59df8
dmitry sokolov <gooflk.53@gmail.com>
2025-07-08 01:08:35 +0300

    добавили функцию sort_by_date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.widget import mask_account_card, get_date
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 # card_name = "Visa Gold 5999414228426353"
 #
@@ -10,4 +10,4 @@ from src.processing import filter_by_state
 # print(get_date(long_date))
 
 source_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-print(filter_by_state(source_list, 'CANCELED'))
+print(sort_by_date(source_list, False))
--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,9 @@ def filter_by_state(source_list: list[dict], state='EXECUTED') -> list[dict]:
     return exit_list
 
 
+def sort_by_date(source_list: list[dict], sort_order=True) -> list[dict]:
+    """принимает список словарей и возвращает список, отсортированный по дате в нужном порядке"""
+
+    exit_list = sorted(source_list, key=lambda x: x['date'], reverse=sort_order)
+
+    return exit_list
